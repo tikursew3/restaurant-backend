616f723fed4e63def9bc36060cab3a4051e521f6
Menelik Tawye <tikursew3@gmail.com>
2025-08-08 19:15:01 -0500

    chore: initial commit for restaurant backend

---
+++ src/main/java/com/passion2code/restaurant_backend/RestaurantBackendApplication.java
@@ -0,0 +1,15 @@
+package com.passion2code.restaurant_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantBackendApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(RestaurantBackendApplication.class, args);
+	}
+
+}
+
